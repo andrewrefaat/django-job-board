d914717e5d10240676536d7c3ebaafb4e34ded0c
andrewrefaat <67602240+andrewrefaat@users.noreply.github.com>
2020-07-06 21:03:58 +0400

    add job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.db.models import CharField
 # Create your models here.
+
+ 
+JOB_TYBE = (
+    ("full Time","Full Time"),
+    ("Part Time","Part Time"),  
+)
+
+
+class job(models.Model):
+    title = models.CharField(max_length=100)
+    JOB_TYBE = models.CharField(max_length=15, choices=JOB_TYBE)
+    Description = models.TextField(max_length=1000)
+    Published_at = models.DateTimeField(auto_now=True)
+    Vacancy = models.IntegerField(default=1)
+    Salary = models.IntegerField(default=0)
+    Experience = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #my apps
+    'job',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'project.urls'
